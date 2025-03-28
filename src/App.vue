d604f0b41ec6e0a2fd3c9939952f2476b681aed4
Gyanielloyd <38559993+Gyanielloyd@users.noreply.github.com>
2025-03-28 18:49:21 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,7 @@ export default {
           "code": "3d6ca0e0-e73d-4f0c-b161-09a28535bf3b//7c169109-15ea-4d65-8165-23a9994ff6ae",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Peer To Peer Marketing"
+          "mech": "Prey"
         },
         {
           "callsign": "Ender",
